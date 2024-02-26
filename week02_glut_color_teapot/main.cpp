eaccea913f329876b8bcddba7dda6e7b613da06c
yuting8426 <112918648+yuting8426@users.noreply.github.com>
2024-02-26 16:41:29 +0800

    week02

---
+++ week02_glut_color_teapot/main.cpp
@@ -0,0 +1,21 @@
+#include <GL/glut.h>
+void display()
+{
+    glColor3f(247/255.0, 180/255.0, 171/255.0);
+    glutSolidTeapot(0.3);
+
+    glColor3f(251/255.0, 233/255.0, 219/255.0);
+    glutSolidTeapot(0.2);
+    glutSwapBuffers();
+}
+
+int main(int argc,char*argv[])
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week02_glut_color_teapot");
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
+
+
