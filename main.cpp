1223b029e8a43271851dcec43fd2329fa485f422
yuting8426 <112918648+yuting8426@users.noreply.github.com>
2024-03-25 16:28:55 +0800

    week06

--- main.cpp
+++ main.cpp
@@ -1,44 +1,34 @@
-#include <opencv/highgui.h>
-#include <opencv/cv.h>
 #include <GL/glut.h>
-int myTexture(char * filename)
+#include "glm.h"
+GLMmodel*pmodel=NULL;
+void drawmodel(void)
 {
-    IplImage * img = cvLoadImage(filename); ///OpenCV讀圖
-    cvCvtColor(img,img, CV_BGR2RGB); ///OpenCV轉色彩 (需要cv.h)
-    glEnable(GL_TEXTURE_2D); ///1. 開啟貼圖功能
-    GLuint id; ///準備一個 unsigned int 整數, 叫 貼圖ID
-    glGenTextures(1, &id); /// 產生Generate 貼圖ID
-    glBindTexture(GL_TEXTURE_2D, id); ///綁定bind 貼圖ID
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); /// 貼圖參數, 超過包裝的範圖T, 就重覆貼圖
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); /// 貼圖參數, 超過包裝的範圖S, 就重覆貼圖
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); /// 貼圖參數, 放大時的內插, 用最近點
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); /// 貼圖參數, 縮小時的內插, 用最近點
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->width, img->height, 0, GL_RGB, GL_UNSIGNED_BYTE, img->imageData);
-    return id;
+    if (!pmodel) {
+	pmodel = glmReadOBJ("data/porsche.obj");
+	if (!pmodel) exit(0);
+	glmUnitize(pmodel);
+	glmFacetNormals(pmodel);
+	glmVertexNormals(pmodel, 90.0);
+    }
+    glmDraw(pmodel, GLM_SMOOTH | GLM_MATERIAL);
 }
-float angle=0;
-GLUquadric * quad =NULL;
 void display()
 {
-    glEnable(GL_DEPTH_TEST);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glPushMatrix();
-        glRotatef(90,1,0,0);    //球的轉的角度
-        glRotatef(angle++,0,1,0);
-        gluSphere(quad,1,30,30);  //glutSolidTeapot( 0.3 );
-    glPopMatrix();
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    drawmodel();
     glutSwapBuffers();
 }
-int main(int argc, char**argv)
+int main(int argc,char*argv[])
 {
-    glutInit( &argc, argv );
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("week09 texture");
-    glutIdleFunc(display);
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("GLUT Shapes");
     glutDisplayFunc(display);
-    myTexture("c:/earth.jpg");
-    quad=gluNewQuadric();
-    myTexture("c:/earth.jpg");
-    gluQuadricTexture(quad,1);
     glutMainLoop();
 }
+
+
+
+
+
+
